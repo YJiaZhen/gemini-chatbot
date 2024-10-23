745bb6078b27810a9d17b9653e03d87af9ae27a1
楊家蓁 <yjz@xuyingchaos-Mac-mini.local>
2024-10-23 11:25:47 +0800

    調整設定

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,14 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 config({
-  path: ".env.local",
+  path: ".env",
 });
 
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./lib/drizzle",
   dialect: "postgresql",
+  
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
